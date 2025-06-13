97f68831c85736094da80fa41d11c83fb884a401
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-06-13 12:37:59 -0300

    Crea proyecto con spring initializer

---
+++ src/main/java/ar/com/emanar/WsRestServerApplication.java
@@ -0,0 +1,13 @@
+package ar.com.emanar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/emanar/WsRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.emanar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

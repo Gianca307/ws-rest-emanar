73dc74eb1ddfe7f7bf0ad29177f4fd4198e8c7d6
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-06-29 10:08:09 -0300

    Agrega ToString en Producto

--- src/main/java/ar/com/emanar/domain/Producto.java
+++ src/main/java/ar/com/emanar/domain/Producto.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
@@ -59,6 +60,7 @@ public class Producto {
 	
 	@ManyToOne
 	@JoinColumn(name = "categoriaProducto_id", referencedColumnName = "id")
+	@ToString.Exclude
 	private CategoriaProducto categoriaProducto;
 	
 }

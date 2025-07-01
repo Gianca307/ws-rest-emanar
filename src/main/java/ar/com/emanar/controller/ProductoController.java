8dd358617f1d8944126b3b092096c232e0727173
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-07-01 11:03:52 -0300

    Modifica deleteById de ProductoService

--- src/main/java/ar/com/emanar/controller/ProductoController.java
+++ src/main/java/ar/com/emanar/controller/ProductoController.java
@@ -16,38 +16,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.emanar.domain.Producto;
 import ar.com.emanar.service.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
+@Tag(name = "Productos", description = "Operaciones sobre productos")
 @AllArgsConstructor
 @RequestMapping("/api/v1")
 public class ProductoController {
 	private ProductoService productoService;
 	
+	@Operation(summary = "Obtiene producto por ID", description = "Devuelve un producto, identificado por su ID.")
 	@GetMapping("/producto/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Producto findById (@PathVariable Long id) {
 		return this.productoService.findById(id);
 	}
 	
+	@Operation(summary = "Obtiene todos los productos", description = "Devuelve una lista de productos.")
 	@GetMapping("/productos")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Producto> findAll (){
 		return this.productoService.findAll();
 	} 
 	
+	@Operation(summary = "Guarda un producto", description = "Persiste un producto en el sistema.")
 	@PostMapping("/producto")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto save(@RequestBody Producto producto, @RequestParam Long idCategoriaProducto) {
 		return this.productoService.save(producto, idCategoriaProducto);
 	}
 	
+	@Operation(summary = "Modifica un producto por su ID", description = "Actualiza un producto en el sistema.")
 	@PutMapping("/producto/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto update (@RequestBody Producto producto, @RequestParam Long idCategoriaProducto, @PathVariable Long id) {
 		return this.productoService.update(producto, idCategoriaProducto, id);
 	}
 	
+	@Operation(
+			summary = "Elimina un producto por su ID", 
+			description = "Elimina un producto, identificado por su ID. " + 
+			"Elimina de los productos comprados y vendidos la referencia vinculada al id del producto.")
 	@DeleteMapping("/producto/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteById(@PathVariable Long id) {

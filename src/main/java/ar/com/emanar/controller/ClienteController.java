17fab7f954c52c7a90fad38586d724b35f4073f7
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-07-01 10:11:33 -0300

    .

--- src/main/java/ar/com/emanar/controller/ClienteController.java
+++ src/main/java/ar/com/emanar/controller/ClienteController.java
@@ -15,38 +15,51 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.emanar.domain.Cliente;
 import ar.com.emanar.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
+@Tag(name = "Clientes", description = "Operaciones sobre clientes")
 @AllArgsConstructor
 @RequestMapping("/api/v1")
 public class ClienteController {
 	private ClienteService clienteService;
 	
+	@Operation(summary = "Obtener Cliente por ID", description = "Devuelve una Cliente, identificada por su id.")
 	@GetMapping("/cliente/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Cliente findById(@PathVariable Long id) {
 		return this.clienteService.findById(id);
 	}	
 	
+	@Operation(summary = "Obtener todos los clientes", description = "Devuelve una lista de Clientes.")
 	@GetMapping("/clientes")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Cliente> findAll(){
 		return this.clienteService.findAll();
 	}
 	
+	@Operation(summary = "Guarda en el sistema un cliente")
 	@PostMapping("/cliente")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente save (@RequestBody Cliente cliente) {
 		return this.clienteService.save(cliente);
 	}
 	
+	@Operation(
+			summary = "Modifica un cliente por su id", 
+			description = "Modifica un cliente existente en el sistema, identificada por su ID.")
 	@PutMapping("/cliente/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente update (@RequestBody Cliente cliente, @PathVariable Long id) {
 		return this.clienteService.update(cliente, id);
 	}
 	
+	@Operation(
+			summary = "Elimina un cliente por su id", 
+			description = "Elimina un cliente existente en el sistema, identificada por su ID. " + 
+			"También elimina de las ventas la referencia vinculadas al id del cliente.")
 	@DeleteMapping("/cliente/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteById (@PathVariable Long id) {
--- src/main/java/ar/com/emanar/service/GastoService.java
+++ src/main/java/ar/com/emanar/service/GastoService.java
@@ -1,8 +1,6 @@
 package ar.com.emanar.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;

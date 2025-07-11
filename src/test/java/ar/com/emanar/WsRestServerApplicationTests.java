e060a4fa49bb82d72b3e00fef6561ba3471d3b3b
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-07-11 11:14:31 -0300

    .

--- src/test/java/ar/com/emanar/WsRestServerApplicationTests.java
+++ src/test/java/ar/com/emanar/WsRestServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WsRestServerApplicationTests extends BaseTest{
+class WsRestServerApplicationTests{
 
 	@Test
 	void contextLoads() {

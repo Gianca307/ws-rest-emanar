8acbe8d122b12871be4daf1c75d4e70662228be1
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-08-20 20:11:46 -0300

    Modifica Security Config
    Agrega configuración cors para el front
    Modifica permisos de endpoint

--- src/main/java/ar/com/emanar/config/SecurityConfig.java
+++ src/main/java/ar/com/emanar/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package ar.com.emanar.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +13,9 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,11 +33,32 @@ public class SecurityConfig {
 				.csrf(AbstractHttpConfigurer::disable)
 				.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/auth/**", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/v1/productos").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/v1/producto/**").permitAll()
+						.requestMatchers("/auth/**",
+								"/h2-console/**",
+								"/swagger-ui/**",
+								"/v3/api-docs/**",
+								"/swagger-ui.html").permitAll()
 						.anyRequest().authenticated())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authenticationProvider(authProvider)
 				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class) //encargado de auntenticar las credenciales
 				.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		
+		// 👇 acá definís quién puede consumir tu API
+		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true); // necesario si usás cookies / Authorization headers
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }

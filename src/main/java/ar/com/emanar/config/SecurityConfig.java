9784c44d740e5fef6f8aabb6886e693bf56aaaae
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-08-21 14:30:06 -0300

    Modifica Security Config

--- src/main/java/ar/com/emanar/config/SecurityConfig.java
+++ src/main/java/ar/com/emanar/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		
 		// 👇 acá definís quién puede consumir tu API
-		configuration.setAllowedOrigins(List.of("http://localhost:5173/"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true); // necesario si usás cookies / Authorization headers

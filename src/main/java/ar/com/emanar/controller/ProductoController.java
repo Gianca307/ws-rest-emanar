bceb7bfdb3b3944bafa94c80462d21a4a6475fe5
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-08-21 20:58:17 -0300

    Agrega cors a endpoint de productos

--- src/main/java/ar/com/emanar/controller/ProductoController.java
+++ src/main/java/ar/com/emanar/controller/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ public class ProductoController {
 	
 	@Operation(summary = "Obtiene todos los productos", description = "Devuelve una lista de productos.")
 	@GetMapping("/productos")
+	@CrossOrigin(origins = {"http://localhost:5173" , "http://127.0.0.1:5173", "https://silly-custard-9f91d4.netlify.app"})
 	@ResponseStatus(HttpStatus.OK)
 	public List<Producto> findAll (){
 		return this.productoService.findAll();
